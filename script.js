aecec63c790c953302f8f040001de58fbec74e8c
SrManinka <gbaalmeida@hotmail.com>
2025-01-18 18:35:21 -0300

    robot svg

--- script.js
+++ script.js
@@ -4,35 +4,38 @@ var app = new Vue({
     projects: [
       {
         name: "Agendamento de Provas",
-        description: "Página Web para gerar avaliações no sistema Prova Fácil da forma mais rápida e simples possível.",
-        techs: ["Js", "Node.js", "Vuejs", " HTML", "CSS"],
+        description:
+          "Página Web para gerar avaliações no sistema Prova Fácil da forma mais rápida e simples possível.",
+        techs: ["Nodejs","ExpressJs", "HTML",  "JS", "CSS"],
       },
       {
         name: "Gerador de Documentos",
-        description: "Plataforma web para criar certificados em massa e salvar no Google Drive. ",
-        techs: ["Js", "Node.js", "Vuejs", " HTML", "CSS"],
+        description:
+          "Plataforma web para criar certificados em massa e salvar no Google Drive. ",
+        techs: ["Nodejs", "Vuejs", "HTML", "JS", "CSS"],
       },
       {
         name: "Vídeo tracker",
-        description: "Programa para buscar os vídeos de uma determinada pasta e salvar o nome, o tempo de cada e o total de horas em uma planilha.",
+        description:
+          "Programa para buscar os vídeos de uma determinada pasta e salvar o nome, o tempo de cada e o total de horas em uma planilha.",
         techs: ["Python"],
       },
 
       {
         name: "Carregamento de Aulas na Plataforma",
-        description: "Automação que busca as aulas no Google Drive, salva na plataforma de vídeos e insere nas salas de aula dentro do ambiente virtual de aprendizagem.",
-        techs: ["Js", "Node.js", "Puppeteer"],
+        description:
+          "Automação que busca as aulas no Google Drive, salva na plataforma de vídeos e insere nas salas de aula dentro do ambiente virtual de aprendizagem.",
+        techs: ["Nodejs", "Puppeteer"],
       },
     ],
-    showProjects: false
+    showProjects: true,
   },
   delimiters: ["${", "}"], // Avoid Twig conflicts
   methods: {
     async toggleProjects() {
-      this.showProjects= !this.showProjects
-console.log(this.showProjects)
+      this.showProjects = !this.showProjects;
+      console.log(this.showProjects);
     },
-  }, mounted() {
-  }
+  },
+  mounted() {},
 });
-
